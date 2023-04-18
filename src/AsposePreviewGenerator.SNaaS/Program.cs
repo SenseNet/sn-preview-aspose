476db2c70d56eb02bf14ddf7da53885955b31984
tusmester <tusmester@gmail.com>
2023-04-18 18:43:53 +0200

    Add preview genreator for snaas.

---
+++ src/AsposePreviewGenerator.SNaaS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using AsposePreviewGenerator.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SenseNet.Extensions.DependencyInjection;
+using Serilog;
+using SNaaS.Client.Security;
+using SNaaS.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using SenseNet.TaskManagement.Core;
+
+namespace SenseNet.Preview.Aspose.AsposePreviewGenerator
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            await PreviewGenerator.ExecuteAsync(args, host.Services).ConfigureAwait(false);
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hb, services) => services
+                    .AddLogging(logging =>
+                    {
+                        logging.AddSerilog(new LoggerConfiguration()
+                            .ReadFrom.Configuration(hb.Configuration)
+                            .CreateLogger());
+                    })
+                    .Configure<AsposePreviewGeneratorOptions>(options =>
+                    {
+                        hb.Configuration.GetSection("sensenet:AsposePreviewGenerator").Bind(options);
+
+                        var environmentName = hb.Configuration["NETCORE_ENVIRONMENT"] ?? "Production";
+                        options.Environment.IsDevelopment = string.Equals(environmentName, "Development",
+                                                       StringComparison.InvariantCultureIgnoreCase);
+                    })
+                    .AddSingleton<ISnClientProvider, SNaaSClientProvider>()
+                    .ConfigureSnaasOptions(hb.Configuration)
+                    .AddSnaasSecretStore()
+                    .AddSenseNetPreview()
+                    .AddSenseNetAsposePreviewGenerators()
+                    .AddSenseNetRetrier());
+    }
+}
---
+++ src/AsposePreviewGenerator.SNaaS/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a4d32605-1991-4023-ab2a-6708f08c9dc9")]
